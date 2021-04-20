5a4b9a0a374bec029cd236b0296403e1977220ee
SaeedBigHero <saed01151599985@gmail.com>
2021-04-20 12:49:53 +0200

    re originis the architure

--- python_lab/python_lab.py
+++ python_lab/python_lab.py
@@ -287,3 +287,26 @@ except:
        print("invalid") 
        
 '''
+
+
+#Q17.
+
+import time
+
+# starting time
+start = time.time()
+
+# program body starts
+for i in range(10):
+    print(i)
+
+# sleeping for 1 sec to get 10 sec runtime
+time.sleep(1)
+
+# program body ends
+
+# end time
+end = time.time()
+
+# total time taken
+print(f"Runtime of the program is {end - start}")
