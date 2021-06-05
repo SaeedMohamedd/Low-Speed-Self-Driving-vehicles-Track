ebd5a5876806e338338daaf4d46455052ed1f8b2
SaeedBigHero <saed01151599985@gmail.com>
2021-06-05 14:39:43 +0200

    add avoid obscale task

--- avoid_obstacle/avoid_obstacle/override_cmd.py
+++ avoid_obstacle/avoid_obstacle/override_cmd.py
@@ -14,6 +14,7 @@ class my_node (Node):
         self.create_subscription(Twist,"/key_cmd_vel",self.bridge_call,10)
         self.create_subscription(LaserScan,"/scan",self.scan_call,10)   
         self.vel_pub=self.create_publisher(Twist,"cmd_vel",10) 
+        self.create_timer(1/5,self.check_call)
         self.get_logger().info("subscriber is started")
         self.stop_msg=Twist()
         self.stop_msg.linear.x=0.0
@@ -22,23 +23,46 @@ class my_node (Node):
         self.stop_msg.angular.x=0.0
         self.stop_msg.angular.y=0.0
         self.stop_msg.angular.z=0.0
-        self.laser_data=[1,2]
-        
-    def bridge_call(self,msg):
-        x=msg.linear.x
-        print(self.laser_data)
-        print(self.laser_data.index(min(self.laser_data)), min(self.laser_data)) 
-        if min(self.laser_data) < 2 and x > 0:
+        self.laser_data=[1]
+        self.x=0
+        self.key_vel_cmd=Twist()
+
+
+    def check_call(self):
+        count=0
+        my_min_list=[]
+        for i in self.laser_data:
+            if count < 20 or count > 340 :
+                my_min_list.append(i)
+            count+=1
+            if count >359:
+                break
+          
+        if min(my_min_list) < 1:
+            self.vel_pub.publish(self.stop_msg)
+        else:
+            self.vel_pub.publish(self.key_vel_cmd)
+
             
-            if  self.laser_data.index(min(self.laser_data)) < 20 or  self.laser_data.index(min(self.laser_data)) > 340:
+        '''
+        if min(self.laser_data) < .7 and self.x > 0:
+            #if  self.laser_data.index(min(self.laser_data)) < 5 or  self.laser_data.index(min(self.laser_data)) > 350:
+            if  self.laser_data.index(min(self.laser_data)) < 10 :
                 self.vel_pub.publish(self.stop_msg)
         else:
-            self.vel_pub.publish(msg)
+            self.vel_pub.publish(self.key_vel_cmd)
+        '''
+
+
+
+    def bridge_call(self,msg):
+        self.key_vel_cmd=msg
+        self.x=msg.linear.x
 
 
     def scan_call(self,msg):
         self.laser_data=msg.ranges
-        #print(self.laser_data.index(min(self.laser_data)), min(self.laser_data))
+        print(self.laser_data.index(min(self.laser_data)), min(self.laser_data))
 
 
 
